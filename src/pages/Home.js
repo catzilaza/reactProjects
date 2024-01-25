a48ccc8db92b549878bfdf0f37282265b7e8a013
Ariya <49780099+catzilaza@users.noreply.github.com>
2024-01-26 02:26:22 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
 
   async function getdata() {
     await axios
-      .get('http://localhost:3000/product')
+      .get('https://crabby-teal-seal.cyclic.app/product')
       .then((response) => {
         setData(response.data);        
         //console.log(response.data);`http://localhost:3000/product`
